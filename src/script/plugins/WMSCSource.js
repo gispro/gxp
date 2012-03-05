b359b3562ae218d61a62991347a211db0ac7a170
Kirill Jakovlev <special-k@li.ru>
2012-03-05 11:16:19 +0400

    added fields resolutions, minResolution, maxResolution, wgsMaxExtentArray in rec

--- src/script/plugins/WMSCSource.js
+++ src/script/plugins/WMSCSource.js
@@ -73,7 +73,7 @@ gxp.plugins.WMSCSource = Ext.extend(gxp.plugins.WMSSource, {
         gxp.plugins.WMSCSource.superclass.constructor.apply(this, arguments); 
         this.format = new OpenLayers.Format.WMSCapabilities({profile: 'WMSC'});
     },
-    
+
     /** api: method[createLayerRecord]
      *  :arg config:  ``Object``  The application config for this layer.
      *  :returns: ``GeoExt.data.LayerRecord``
@@ -81,12 +81,59 @@ gxp.plugins.WMSCSource = Ext.extend(gxp.plugins.WMSSource, {
      *  Create a layer record given the config.
      */
     createLayerRecord: function(config) {
-        var record = gxp.plugins.WMSCSource.superclass.createLayerRecord.apply(this, arguments);
+        var original = gxp.plugins.WMSCSource.superclass.createLayerRecord.apply(this, arguments);
+        var record = original
         var caps = this.store.reader.raw.capability;
+        //console.log(caps)
         var tileSets = (caps && caps.vendorSpecific && caps.vendorSpecific) ? 
             caps.vendorSpecific.tileSets : null;
         if (tileSets !== null) {
-            var layer = record.get("layer");
+
+            // add additional fields
+            var fields = [
+                {name: "resolutions" }, //Array
+                {name: "minResolution" }, //Array
+                {name: "maxResolution" }, //Array
+                {name: "wgsMaxExtentArray" } //Array
+            ];
+
+            original.fields.each(function(field) {
+                fields.push(field);
+            });
+
+            var resolutions = {}, minResolution = {}, maxResolution = {}, wgsMaxExtentArray = null
+
+            for (var i=0, len=tileSets.length; i<len; i++) {
+                var tileSet = tileSets[i];
+                if( config.name == tileSet.layers ){
+                  var srs
+                  for( var k in tileSet.srs) srs = k
+                  resolutions[srs] = tileSet.resolutions
+                  minResolution[srs] = tileSet.resolutions[0]
+                  maxResolution[srs] = tileSet.resolutions[tileSet.resolutions.length-1]
+                  //maxExtent[srs] = new OpenLayers.Bounds.fromArray( tileSet.bbox[srs].bbox )
+                }
+            }
+
+            for (var i=0, len=caps.layers.length; i<len; i++) {
+                var layer = caps.layers[i];
+                if( config.name == layer.name ){
+                  wgsMaxExtentArray = layer.bbox["EPSG:4326"].bbox
+                }
+            }
+
+            var data = Ext.applyIf({
+                resolutions: resolutions,
+                minResolution: minResolution,
+                maxResolution: maxResolution,
+                wgsMaxExtentArray: wgsMaxExtentArray
+            }, original.data);
+
+            var layer = original.get("layer");
+
+            var Record = GeoExt.data.LayerRecord.create(fields);
+            record = new Record(data, layer.id);
+
             var mapProjection = this.getMapProjection();
             // look for tileset with same name and equivalent projection
             for (var i=0, len=tileSets.length; i<len; i++) {
@@ -101,7 +148,7 @@ gxp.plugins.WMSCSource = Ext.extend(gxp.plugins.WMSSource, {
                         var bbox = tileSet.bbox[srs].bbox;
                         layer.projection = tileProjection;
                         layer.addOptions({
-                            resolutions: tileSet.resolutions,
+                            //resolutions: tileSet.resolutions,
                             tileSize: new OpenLayers.Size(tileSet.width, tileSet.height),
                             tileOrigin: new OpenLayers.LonLat(bbox[0], bbox[1])
                         });
@@ -129,7 +176,7 @@ gxp.plugins.WMSCSource = Ext.extend(gxp.plugins.WMSSource, {
             cached: !!record.getLayer().params.TILED
         });
     }
-    
+
 });
 
 Ext.preg(gxp.plugins.WMSCSource.prototype.ptype, gxp.plugins.WMSCSource);
