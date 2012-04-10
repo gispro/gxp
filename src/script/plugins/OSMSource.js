d10498cac4ee09364eb338d77b1d0aa69f24ac8d
Kirill Jakovlev <special-k@li.ru>
2012-04-10 13:30:06 +0400

    revert changes

--- src/script/plugins/OSMSource.js
+++ src/script/plugins/OSMSource.js
@@ -91,8 +91,7 @@ gxp.plugins.OSMSource = Ext.extend(gxp.plugins.LayerSource, {
             maxResolution: 156543.03390625,
             numZoomLevels: 19,
             units: "m",
-            buffer: 1,
-            isBaseLayer: false
+            buffer: 1
         };
         
         var layers = [
